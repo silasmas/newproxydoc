e3e02c3005ebabd905b74e50b4925f8d45cc764c
silasmas <silasjmas@gmail.com>
2022-07-29 02:54:23 +0100

    dsdd

--- app/Http/Controllers/AchatController.php
+++ app/Http/Controllers/AchatController.php
@@ -243,6 +243,7 @@ class AchatController extends Controller
     public function retour(Request $request)
     {
         $retour = produitUser::where("transaction_id", $request->cpm_trans_id)->first();
+        dd($retour);
         $paiement = order::where("transaction_id", $request->cpm_trans_id)->first();
        // $ab = abonnement::with("service")->where("id", $retour->abonnement_id)->first();
         if ($retour) {
